66ce42ae346a04a687b17e2516ae06498a8b7aaf
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2021-03-31 12:22:51 +0100

    installed and added style-loader and css-loader to webpack.config.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
-import { handleSubmit } from './js/formHandler';
-import { checkUrl } from './js/checkUrl';
+import { handleSubmit } from "./js/formHandler";
+import { checkUrl } from "./js/checkUrl";
+import "./styles/main.css";
 
 console.log('Hello from Webpack');
