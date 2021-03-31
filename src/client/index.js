b9d1724369a0773c0b9ae1df69c2aceeb4161be2
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2021-03-31 11:30:51 +0100

    updated initial webpack configs, created webpack.config.js and updated some code

---
+++ src/client/index.js
@@ -0,0 +1,4 @@
+import { formHandler } from './js/formHandler';
+import { checkUrl } from './js/checkUrl';
+
+console.log('Hello from Webpack');
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,5 +1,7 @@
 function handleSubmit(event) {
     event.preventDefault();
-    let formUrl = document.getElementById('url').nodeValue;
+    let formUrl = document.getElementById('url').value;
     console.log(formUrl);
 }
+
+export {handleSubmit}
