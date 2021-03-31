fabd87368662bd9317788c20c8dbdcdc684e3477
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2021-03-31 23:24:32 +0100

    installed webpack-dev-server and updated configuration on package.json file for it to work on webpack.dev.js file.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,9 @@
 const path = require("path");
-const config = require("./webpack.config");
+const common = require("./webpack.common");
 const {merge} = require("webpack-merge");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = merge(config, {
+module.exports = merge(common, {
     mode: "development",
     
     plugins: [new HtmlWebpackPlugin({
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,9 @@
 const path = require("path");
-const config = require("./webpack.config");
+const common = require("./webpack.common");
 const {merge} = require("webpack-merge");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
-module.exports = merge(config, {
+module.exports = merge(common, {
     mode: "production",
     
     plugins: [new HtmlWebpackPlugin({
