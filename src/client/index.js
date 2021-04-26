6b661ccce94efb8f71e9a875fc145af1cf98a544
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2021-04-26 16:41:13 +0100

    feat: Finished client and server side JS code, selecting the data i want to appear on the page as a result. Feat: started styling, added fonts to headers and body. Will tweak them. ToDo: Re-arrange the sizes, margins, and most of the visual part of the content. Maybe add some color.

--- src/client/index.js
+++ src/client/index.js
@@ -4,6 +4,7 @@ import { checkUrl } from "./js/checkUrl";
 import "./styles/base.scss";
 import "./styles/footer.scss";
 import "./styles/form.scss";
+import "./styles/section.scss";
 import "./styles/header.scss";
 
 
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,6 +3,7 @@ import {checkUrl} from "./checkUrl";
 
 
 document.getElementById('submit').addEventListener('click', handleSubmit);
+document.getElementById('reset').addEventListener('click', pageReset);
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -18,22 +19,20 @@ function handleSubmit(event) {
                 },
                 body:JSON.stringify({formUrl: formUrl}),
             })    
-            .then(res => {
+            .then(function(res) {
                 return res.json()
             })
             .then(function(data) {
                 console.log(data);
                 document.getElementById('report').style.display = "block";
-                document.getElementById('agreement').innerHTML = data.agreement
-                document.getElementById('confidence').innerHTML = data.confidence
-                document.getElementById('irony').innerHTML = data.irony
-                document.getElementById('model').innerHTML = data.model
-                document.getElementById('score').innerHTML = data.score_tag
+                document.getElementById('agreement').innerHTML = "<p><strong>Agreement:</strong></p>" + data.agreement
+                document.getElementById('confidence').innerHTML = "<p><strong>Confidence:</strong></p>" + data.confidence
+                document.getElementById('irony').innerHTML = "<p><strong>Irony:</strong></p>" + data.irony
+                document.getElementById('score').innerHTML = "<p><strong>Score:</strong></p>" + data.score_tag
             })
         console.log(":::Form Submitted:::");
             
         } else {
-                alert('invalid URL')
                 console.log(formUrl)
                 errorMessage()
             }  
@@ -42,9 +41,16 @@ function handleSubmit(event) {
 //need to change this, it is creating a new paragraph on each wrong submission instead of removing and updating the same one
 function errorMessage() {
     const validator = document.getElementById('url_validation');
-    validator.innerHTML = "<p>URL is not valid, please use a correct URL and try again</p>";
-       
+    validator.innerHTML = "<p>URL is not valid, please use a correct URL and try again</p>"; 
 };   
+
+
+function pageReset() {
+    const validator = document.getElementById('url_validation');
+    validator.innerHTML = '';
+    document.getElementById('report').style.display = "none";
+}
+
 
 
 
