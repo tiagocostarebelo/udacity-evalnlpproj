5d3153722c0ba5cbd29fd217dd08ade6d4efc84f
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2021-04-28 11:33:30 +0100

    style: Formatted and styled all elements, including the results coming from the API fetch.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,11 +4,15 @@ import {checkUrl} from "./checkUrl";
 
 document.getElementById('submit').addEventListener('click', handleSubmit);
 document.getElementById('reset').addEventListener('click', pageReset);
+const validator = document.getElementById('url_validation');
 
 function handleSubmit(event) {
     event.preventDefault();
     let formUrl = document.getElementById('url').value
-        if(checkUrl(formUrl)) {
+    validator.innerHTML = '';
+    document.getElementById('report').style.display = "none";
+
+        if(checkUrl(formUrl)) {            
             console.log(formUrl)
             fetch('http://localhost:8001/analysis', {
                 method: 'POST',
@@ -26,10 +30,10 @@ function handleSubmit(event) {
                 console.log(data);
                 
                 document.getElementById('report').style.display = "block";
-                document.getElementById('agreement').innerHTML = "<p><strong>Agreement:</strong></p>" + data.agreement
-                document.getElementById('confidence').innerHTML = "<p><strong>Confidence:</strong></p>" + data.confidence
-                document.getElementById('irony').innerHTML = "<p><strong>Irony:</strong></p>" + data.irony
-                document.getElementById('score').innerHTML = "<p><strong>Score:</strong></p>" + data.score_tag
+                document.getElementById('agreement').innerHTML = "<p>Agreement:</p>" + data.agreement
+                document.getElementById('confidence').innerHTML = "<p>Confidence:</p>" + data.confidence
+                document.getElementById('irony').innerHTML = "<p>Irony:</p>" + data.irony
+                document.getElementById('score').innerHTML = "<p>Score:</p>" + data.score_tag
             })
         console.log(":::Form Submitted:::");
             
@@ -40,8 +44,7 @@ function handleSubmit(event) {
 };
 
 //need to change this, it is creating a new paragraph on each wrong submission instead of removing and updating the same one
-function errorMessage() {
-    const validator = document.getElementById('url_validation');
+function errorMessage() {    
     validator.innerHTML = "<p>URL is not valid, please use a correct URL and try again</p>"; 
 };   
 
