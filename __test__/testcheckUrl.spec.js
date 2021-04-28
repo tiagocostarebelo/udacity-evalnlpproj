96ec6ff1e35c33a3b7bccee82c62df1d042e2447
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2021-04-28 22:02:33 +0100

    Updated folder structure to redo the Jest tests as i made a couple mistakes.

--- __test__/testcheckUrl.spec.js
+++
@@ -1,6 +0,0 @@
-import { checkUrl } from "../src/client/js/checkUrl";
-
-describe("Testing to make sure input is url",()=>{
-    test("Testing the checkUrl() function", ()=>{
-       expect(checkUrl("Test")).toBe(false);
-        })})
