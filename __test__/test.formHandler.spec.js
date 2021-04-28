0e7a3fa3bfa883a8f8ae2fad2fb8d5cfb5038107
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2021-04-28 19:48:28 +0100

    updated file names for Jest tests

--- __test__/test.formHandler.spec.js
+++
@@ -1,7 +0,0 @@
-import { handleSubmit } from "../src/client/js/formHandler";
-
-describe("Testing the submit functionality", () => {
-    test("Testing the handleSubmit() function", () => {
-           expect(handleSubmit).toBeDefined();
-    });
-});
